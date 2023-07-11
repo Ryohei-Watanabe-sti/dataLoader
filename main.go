48f98de882d0450d6aa1bbf9db8dd33546d35281
ry-watanabe <ry-watanabe@sios.com>
2023-07-11 15:10:54 +0900

    config.jsonから入力CSVパスを読み込み

--- main.go
+++ main.go
@@ -2,21 +2,45 @@ package main
 
 import (
 	"encoding/csv"
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
 )
 
+type Config struct {
+	Csv1 string `json:"csv1"`
+	Csv2 string `json:"csv2"`
+}
+
+func loadConfig() (*Config, error) {
+	f, err := os.Open("config.json")
+	if err != nil {
+		log.Fatal("loadConfig os.Open err:", err)
+		return nil, err
+	}
+	defer f.Close()
+
+	var cfg Config
+	err = json.NewDecoder(f).Decode(&cfg)
+	return &cfg, err
+}
+
 func getCSV() ([][]string, [][]string) {
-	f, err1 := os.Open("sample1.csv")
-	if err1 != nil {
-		log.Fatal(err1)
+	cnf, err := loadConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	f, err := os.Open(cnf.Csv1)
+	if err != nil {
+		log.Fatal(err)
 	}
 	r1 := csv.NewReader(f)
 
-	g, err2 := os.Open("sample2.csv")
-	if err2 != nil {
-		log.Fatal(err2)
+	g, err := os.Open(cnf.Csv2)
+	if err != nil {
+		log.Fatal(err)
 	}
 	r2 := csv.NewReader(g)
 
@@ -35,5 +59,6 @@ func getCSV() ([][]string, [][]string) {
 
 func main() {
 	csv1, csv2 := getCSV()
-	fmt.Println(csv1, csv2)
+	fmt.Println(csv1)
+	fmt.Println(csv2)
 }
