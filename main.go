2f3f8e91dadb719c0f57e7d2c0a6ddacb8edaa2e
ry-watanabe <ry-watanabe@sios.com>
2023-07-11 14:10:48 +0900

    getCSV関数でメイン関数にCSVスライスを渡す

--- main.go
+++ main.go
@@ -3,32 +3,37 @@ package main
 import (
 	"encoding/csv"
 	"fmt"
-	"io"
 	"log"
 	"os"
 )
 
-func opencsv() {
-	f, err := os.Open("sample1.csv")
+func getCSV() ([][]string, [][]string) {
+	f, err1 := os.Open("sample1.csv")
+	if err1 != nil {
+		log.Fatal(err1)
+	}
+	r1 := csv.NewReader(f)
+
+	g, err2 := os.Open("sample2.csv")
+	if err2 != nil {
+		log.Fatal(err2)
+	}
+	r2 := csv.NewReader(g)
+
+	records1, err := r1.ReadAll()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	r := csv.NewReader(f)
-	for {
-		record, err := r.Read()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println(record)
+	records2, err := r2.ReadAll()
+	if err != nil {
+		log.Fatal(err)
 	}
 
+	return records1, records2
 }
 
 func main() {
-	opencsv()
-	//testtest
+	csv1, csv2 := getCSV()
+	fmt.Println(csv1, csv2)
 }
