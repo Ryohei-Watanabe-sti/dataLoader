b72a39e35bdcbbfeb10305871c59630a041a1923
ry-watanabe <ry-watanabe@sios.com>
2023-07-11 17:48:05 +0900

    createTable関数を作成

--- main.go
+++ main.go
@@ -3,14 +3,24 @@ package main
 import (
 	"encoding/csv"
 	"encoding/json"
-	"fmt"
 	"log"
 	"os"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 type Config struct {
 	Csv1 string `json:"csv1"`
 	Csv2 string `json:"csv2"`
+	Dsn  string `json:"dsn"`
+}
+
+type Table1 struct {
+	gorm.Model
+	ID      int `json:"id"`
+	Name_ja int `json:"name_ja"`
+	Name_en int `json:"name_en"`
 }
 
 func loadConfig() (*Config, error) {
@@ -57,8 +67,29 @@ func getCSV() ([][]string, [][]string) {
 	return records1, records2
 }
 
+func createTable() {
+	cfg, err := loadConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db, err := gorm.Open(mysql.Open(cfg.Dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	createTableQuery := `create table if not exists 
+	table1(
+		id int,
+		name_ja varchar(10),
+		name_en varchar(10)
+	)
+	CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci; `
+	db.Exec(createTableQuery)
+}
+
 func main() {
-	csv1, csv2 := getCSV()
-	fmt.Println(csv1)
-	fmt.Println(csv2)
+	// csv1, csv2 := getCSV()
+	// fmt.Println(csv1)
+	// fmt.Println(csv2)
+	createTable()
 }
