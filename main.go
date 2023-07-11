17548a08f02fe37fe41999b445c3f79945cd62bd
ry-watanabe <ry-watanabe@sios.com>
2023-07-11 11:40:19 +0900

    opencsv関数を追加

--- main.go
+++ main.go
@@ -1,50 +1,33 @@
 package main
 
 import (
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
-    "fmt"
-    "encoding/json"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"os"
 )
 
-type User1 struct {
-    gorm.Model
-	Name        string  `json:"name"`
-    Image_url   string  `json:"image_url"`
-    Email       string  `json:"email"`
-    
+func opencsv() {
+	f, err := os.Open("sample1.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r := csv.NewReader(f)
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(record)
+	}
+
 }
 
 func main() {
-    dsn := "test:12345678@tcp(127.0.0.1:3306)/oh_test?character_set_database=utf8mb4&parseTime=True&loc=Local"
-    db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-    if err != nil {
-		panic("failed to connect database")
-	}
-    up01(dsn, db)
-
-    db.Debug().Create(&User1{Name: "aaa", Image_url: "image.png", Email: "ry-watanabe@sios.com"})
-    
-    var data1 User1
-
-    db.Debug().First(&data1, 1)
-    fmt.Println(data1)
-    read_json, err := json.Marshal(data1)
-    fmt.Println(string(read_json))
-
-    db.Debug().Model(&User1{}).Where("id = ?", 1).Update("Name", "あああ")
-
-    // db.Debug().Delete(&data1, 1)
-
-    down01(dsn, db);
-
-}
-
-func up01(dsn string, db *gorm.DB) {
-    db.AutoMigrate(User1{})
-    fmt.Println("User1テーブルを作成")
-}
-func down01(dsn string, db *gorm.DB) {
-    db.Migrator().DropTable(User1{})
-    fmt.Println("User1テーブルを削除")
+	opencsv()
 }
