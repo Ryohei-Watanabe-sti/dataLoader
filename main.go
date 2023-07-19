8e5f1addee4c11b7f7606e3574c4eb678302f117
ry-watanabe <ry-watanabe@sios.com>
2023-07-19 19:20:14 +0900

    レコードの追加

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"flag"
 	"log"
 	"os"
+	"strconv"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -15,6 +16,12 @@ type Config struct {
 	Csv1 string `json:"csv1"`
 	Csv2 string `json:"csv2"`
 	Dsn  string `json:"dsn"`
+}
+
+func readOption() string {
+	var text = flag.String("t", "tablename", "help message for t")
+	flag.Parse()
+	return *text
 }
 
 func loadConfig() (*Config, error) {
@@ -61,7 +68,7 @@ func getCSV() ([][]string, [][]string) {
 	return records1, records2
 }
 
-func createTable(tName string, columns1 []string, columns2 []string) {
+func createTable(tName string, columns1 [][]string, columns2 [][]string) {
 	cfg, err := loadConfig()
 	if err != nil {
 		log.Fatal(err)
@@ -73,7 +80,11 @@ func createTable(tName string, columns1 []string, columns2 []string) {
 	}
 
 	if db.Migrator().HasTable(tName) == true {
-		panic("failed to create " + tName)
+		panic("failed to create " + tName + ", already exists.")
+	}
+
+	if len(columns1) != len(columns2) {
+		panic("failed to create " + tName + " caused by CSV error.")
 	}
 
 	createTableQuery := `create table ` + tName + ` (id int); `
@@ -81,27 +92,38 @@ func createTable(tName string, columns1 []string, columns2 []string) {
 
 	var addColumnQuery string
 	var cName string
-	for i := 0; i < len(columns1); i++ {
-		cName = columns1[i]
+	for i := 0; i < len(columns1[0]); i++ {
+		cName = columns1[0][i]
 		addColumnQuery = `alter table ` + tName + ` add column csv1_` + cName + ` varchar(255);`
 		db.Exec(addColumnQuery)
 	}
-	for i := 0; i < len(columns2); i++ {
-		cName = columns2[i]
+	for i := 0; i < len(columns2[0]); i++ {
+		cName = columns2[0][i]
 		addColumnQuery = `alter table ` + tName + ` add column csv2_` + cName + ` varchar(255);`
 		db.Exec(addColumnQuery)
 	}
 
-}
+	var insertArr []string
+	var insertQuery string
+	var iString string
+	for i := 1; i < len(columns1); i++ {
+		insertArr = append(columns1[i], columns2[i]...)
+		iString = strconv.Itoa(i)
+		insertQuery = "insert into " + tName + " values (" + iString + ","
 
-func readOption() string {
-	var text = flag.String("t", "tablename", "help message for t")
-	flag.Parse()
-	return *text
+		for j := 0; j < len(insertArr); j++ {
+			insertQuery += "'" + insertArr[j] + "', "
+		}
+		insertQuery = insertQuery[:len(insertQuery)-2]
+		insertQuery += ");"
+		db.Exec(insertQuery)
+		//進捗を表示
+	}
+
 }
 
 func main() {
 	csv1, csv2 := getCSV()
 	var tableName = readOption()
-	createTable(tableName, csv1[0], csv2[0])
+	createTable(tableName, csv1, csv2)
 }
